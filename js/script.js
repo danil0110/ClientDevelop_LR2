e431e0d1982692900e299a65180a9a85b2d9abcd
danil0110 <danilmail0110@gmail.com>
2020-09-18 13:39:53 +0300

    Changes to adding new note function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 function addNote() {
     let note = document.createElement('li');
     note.classList.add('note-single');
-    note.innerHTML = 'My note';
+    let noteName = document.createElement('p');
+    let noteText = document.createElement('p');
+    let noteDate = document.createElement('p');
+    noteName.id = 'note-name-preview';
+    noteText.id = 'note-text-preview';
+    noteDate.id = 'note-date';
+    noteDate.innerHTML = '18.09.2020';
+    note.appendChild(noteName);
+    note.appendChild(noteText);
+    note.appendChild(noteDate);
     document.querySelector('.note-list').appendChild(note);
 }
 
