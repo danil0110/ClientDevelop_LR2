295b4899cc7a4510eeb9f36416a729ff7c01bc62
danil0110 <danilmail0110@gmail.com>
2020-09-20 20:52:12 +0300

    Implemented add function also using array

--- js/script.js
+++ js/script.js
@@ -1,17 +1,42 @@
+var notesArray = new Array;
+
 function addNote() {
-    let note = document.createElement('li');
-    note.classList.add('note-single');
+    let newNote = {
+        name: '',
+        body: '',
+        date: updateDate(),
+    }
+
+    notesArray.unshift(newNote);
     let noteName = document.createElement('p');
     let noteText = document.createElement('p');
     let noteDate = document.createElement('p');
     noteName.id = 'note-name-preview';
     noteText.id = 'note-text-preview';
     noteDate.id = 'note-date';
-    noteDate.innerHTML = '18.09.2020';
-    note.appendChild(noteName);
-    note.appendChild(noteText);
-    note.appendChild(noteDate);
-    document.querySelector('.note-list').appendChild(note);
+    noteDate.innerHTML = newNote.date;
+    let noteElement = document.createElement('li');
+    noteElement.classList.add('note-single');
+    noteElement.appendChild(noteName);
+    noteElement.appendChild(noteText);
+    noteElement.appendChild(noteDate);
+    document.querySelector('.note-list').insertBefore(noteElement, document.querySelector('.note-list').firstChild);
+    console.log(notesArray);
+
+
+    // let note = document.createElement('li');
+    // note.classList.add('note-single');
+    // let noteName = document.createElement('p');
+    // let noteText = document.createElement('p');
+    // let noteDate = document.createElement('p');
+    // noteName.id = 'note-name-preview';
+    // noteText.id = 'note-text-preview';
+    // noteDate.id = 'note-date';
+    // noteDate.innerHTML = '18.09.2020';
+    // note.appendChild(noteName);
+    // note.appendChild(noteText);
+    // note.appendChild(noteDate);
+    // document.querySelector('.note-list').appendChild(note);
 }
 
 function deleteNote() {
@@ -24,12 +49,12 @@ function deleteNote() {
 
 document.getElementById('note-name').oninput = () => {
     document.getElementById('note-name-preview').innerHTML = document.getElementById('note-name').value;
-    updateDate();
+    document.getElementById('note-date').innerHTML = updateDate();
 }
 
 document.getElementById('note-text').oninput = () => {
     document.getElementById('note-text-preview').innerHTML = document.getElementById('note-text').value;
-    updateDate();
+    document.getElementById('note-date').innerHTML = updateDate();
 }
 
 function updateDate() {
@@ -50,5 +75,9 @@ function updateDate() {
     if (minutes < 10) {
         minutes = '0' + minutes;
     }
-    document.getElementById('note-date').innerHTML = day + '.' + month + '.' + date.getFullYear() + ' ' + hours + ':' + minutes;
+    return day + '.' + month + '.' + date.getFullYear() + ' ' + hours + ':' + minutes;
+}
+
+function pageLoaded() {
+
 }
